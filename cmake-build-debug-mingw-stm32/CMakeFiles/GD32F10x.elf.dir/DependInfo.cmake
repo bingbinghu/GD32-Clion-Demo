9d2903b83d028fa11e0df7162a6d3b1a0ebb9ea5
bing hu <1013210434@qq.com>
2022-02-26 20:32:29 +0800

    1. 创建了仓库

---
+++ cmake-build-debug-mingw-stm32/CMakeFiles/GD32F10x.elf.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/startup/startup_gd32f10x_md.S" "D:/HSB/working/HanHan_MCU/GD32/clion_temple/cmake-build-debug-mingw-stm32/CMakeFiles/GD32F10x.elf.dir/startup/startup_gd32f10x_md.S.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "GD32F10X_HD"
+  "USE_STDPERIPH_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/CMSIS"
+  "../UserApp/Inc"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_adc.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_adc.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_adc.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_bkp.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_bkp.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_bkp.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_can.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_can.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_can.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_crc.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_crc.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_crc.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_dac.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_dac.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_dac.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_dbg.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_dbg.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_dbg.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_dma.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_dma.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_dma.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_enet.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_enet.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_enet.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_exmc.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_exmc.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_exmc.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_exti.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_exti.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_exti.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_fmc.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_fmc.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_fmc.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_fwdgt.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_fwdgt.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_fwdgt.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_gpio.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_gpio.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_gpio.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_i2c.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_i2c.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_i2c.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_misc.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_misc.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_misc.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_pmu.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_pmu.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_pmu.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_rcu.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_rcu.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_rcu.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_rtc.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_rtc.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_rtc.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_sdio.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_sdio.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_sdio.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_spi.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_spi.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_spi.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_timer.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_timer.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_timer.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_usart.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_usart.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_usart.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Core/Src/gd32f10x_wwdgt.c" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_wwdgt.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Core/Src/gd32f10x_wwdgt.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/Drivers/CMSIS/system_gd32f10x.c" "CMakeFiles/GD32F10x.elf.dir/Drivers/CMSIS/system_gd32f10x.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/Drivers/CMSIS/system_gd32f10x.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/UserApp/Src/gd32f10x_eval.c" "CMakeFiles/GD32F10x.elf.dir/UserApp/Src/gd32f10x_eval.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/UserApp/Src/gd32f10x_eval.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/UserApp/Src/gd32f10x_it.c" "CMakeFiles/GD32F10x.elf.dir/UserApp/Src/gd32f10x_it.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/UserApp/Src/gd32f10x_it.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/UserApp/Src/main.c" "CMakeFiles/GD32F10x.elf.dir/UserApp/Src/main.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/UserApp/Src/main.c.obj.d"
+  "D:/HSB/working/HanHan_MCU/GD32/clion_temple/UserApp/Src/systick.c" "CMakeFiles/GD32F10x.elf.dir/UserApp/Src/systick.c.obj" "gcc" "CMakeFiles/GD32F10x.elf.dir/UserApp/Src/systick.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
